62925c86c26fb5860ffbfa79fd0439de27667b8e
amytheresamathew16-brg <amytheresamathew16@gmail.com>
2026-04-21 10:07:06 +0530

    Feature(uc3):acceptuserslotinput

---
+++ TicTacToe3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TicTacToe3 {
+
+    // Method to read slot input
+    public static int getPlayerMove() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = sc.nextInt();
+
+        return slot;
+    }
+
+    public static void main(String[] args) {
+        int move;
+
+        System.out.println("=== Tic-Tac-Toe UC3 ===");
+
+        move = getPlayerMove();
+
+        System.out.println("You selected slot: " + move);
+    }
+}
